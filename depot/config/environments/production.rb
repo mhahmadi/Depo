42dc1f45b2bf0b1878eb591cdbf942bec67a79b1
Mohamad Ahmadi <mhahmadi@nteropcorp.com>
2014-07-27 01:26:17 -0400

    Iteration K2
    deployment with Capistrano will not work with these settings. Capistrano
    expects to find the Gemfile in the root of the repository, while out rails
    app (and therefore Gemfile) is under "depo"

--- depot/config/environments/production.rb
+++ depot/config/environments/production.rb
@@ -77,4 +77,8 @@ Depot::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.logger = Logger.new(config.paths['log'].first, 'daily')
+  # Directs the logs to the system logs
+  # config.logger = SyslogLogger.new
 end
